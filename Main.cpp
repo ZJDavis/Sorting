dee5d875b068c56ffc17be67b8219bb32c3b75d5
Zack Davis <zdavis@pdx.edu>
2018-11-28 01:35:46 -0800

    Updated everything
    Added in definitions for each algorithm function.
    Added CountSort, HeapSort and TreeSort cpp files housing their respective algorithm functions
    Modified Main to run nested loops with 10K-10M array sizes and 4 different types of data sets (pseudorandom, near sorted, reverse sorted, and few distinct)
    Fixed time output error in Main
    
    Program is tested and working for the one complete sort function.
    Added in more `cout` lines to clarify what is going on during the run.

--- Main.cpp
+++ Main.cpp
@@ -2,123 +2,100 @@
 
 using namespace std;
 
-//heap sort functions
-void heapify(int * array, int n, int i);
-void heapSort(int * array, int n);
-void countSort(int * arr, int size);
-
 int main() 
 { 
-    int size;
-    cout<< "how big do you want the array?" << endl;
-    cin >> size;
+	Timer timer; //create timer
+	int dataID = 0; //handles the switching between the different data set types
+	cout.precision(15); //avoids the scientific notation on large time values (measured in nanoseconds)
 
-    int*array = new int [size];
+	for (dataID = 1; dataID <= 4; ++dataID)
+    {
+		if (dataID == 1){cout << "Pseudo-random, many distinct:\n";}
+		if (dataID == 2){cout << "Pseudo-random, few distinct:\n";}
+		if (dataID == 3){cout << "Nearly sorted:\n";}
+		if (dataID == 4){cout << "Reverse sorted:\n";}
 
-    srand((unsigned)time(0)); 
+		for (long int size = 10000; size <= 10000000; size = 10 * size)
+		{
+			//create initial array
+			int*array = new int [size];
+			srand((unsigned)time(0)); 
 
-    Timer timer;
-     
-    for(int i=0; i<size; i++)
-    { 
-        array[i] = (rand()%100)+1; 
-    } 
-    cout << endl;
+			//populate array
 
-	//build algorithm arrays here
-    int*arrayQS = new int [size]; //quicksort array
-    int*arrayQS2 = new int [size]; //quicksort array 2
-    int*arrayMS = new int [size]; //merge sort array
-    int*arrayHS = new int [size]; //heapsort array
-    int*arrayCS = new int [size]; //counting sort array
-    int*arrayTS = new int [size]; //treesort array
-    int*array3S = new int [size]; //cubesort array
+			if (dataID == 1) //pseudo-random, many distinct
+			{
+				for(int i=0; i<size; i++)
+				{ 
+					array[i] = (rand() % size)+1; 
+				}
 
-    timer.start();
-    
-	//algorithm calls will go here
-	for (int i=0; i<size-1; i++)
-	{
-		arrayQS[i] = array[i];
+			}
+			else if (dataID == 2) //pseudo-random, few distinct
+			{
+				for(int i=0; i<size; i++)
+				{ 
+					array[i] = (rand() % 100)+1; 
+				} 
+			}
+			else if (dataID == 3) //nearly sorted
+			{
+				for(int i=0; i<size; i++)
+				{ 
+					array[i] = (rand() % 100)+(i/100); 
+				} 
+			}
+			else if (dataID == 4) //reverse sorted
+			{
+				for(int i=0; i<size; i++)
+				{ 
+					array[i] = size - i; 
+				} 
+			}
+			else 
+			{
+				cout << "dataID error.\n";
+			}
+
+			//build algorithm arrays here
+//			int*arrayQS = new int [size]; //quicksort array
+//			int*arrayQS2 = new int [size]; //quicksort array 2
+//			int*arrayMS = new int [size]; //merge sort array
+			int*arrayHS = new int [size]; //heapsort array
+//			int*arrayCS = new int [size]; //counting sort array
+//			int*arrayTS = new int [size]; //treesort array
+//			int*array3S = new int [size]; //cubesort array
+
+			for (int dataPoint=1; dataPoint<=1000; ++dataPoint)
+			{
+				for (int i=0; i<size-1; ++i) //array copy
+				{
+	//				arrayQS[i] = array[i];
+	//				arrayQS2[i] = array[i];
+	//				arrayMS[i] = array[i];
+					arrayHS[i] = array[i];
+	//				arrayCS[i] = array[i];
+	//				arrayTS[i] = array[i];
+	//				array3S[i] = array[i];
+				}
+				//reset timer numbers, just in case
+
+				timer.start();
+				heapSort(arrayHS, size);
+				timer.stop();
+				
+				if (size > 1000000) //nanoseconds will overflow after about 2 seconds
+					cout << "Array size: " << size << ", Time: " << timer.elapsedMilliseconds() << " milliseconds.\n";
+				else
+					cout << "Array size: " << size << ", Time: " << timer.elapsedNanoseconds() << " nanoseconds.\n";
+				/*
+				timer.start();
+				treeSort(arrayTS, size);
+				timer.stop();
+				*/
+			}
+		}
+	// file output will go here
 	}
-
-    timer.stop();
-
-	int tS = timer.elapsedSeconds();
-	int tMS = timer.elapsedMilliseconds();
-	int tNS = timer.elapsedNanoseconds();
-    
-    cout << "Time: " << tS << " seconds \n"; 
-    cout << tMS % 1000 << tNS % 1000000 << " nanoseconds.\n";
-
     return 0;
 }
-
-//heapify creates the heap for heapsort. node i
-//is the root, and n is the size of the heap
-void heapify(int * array, int n, int i)
-{
-    int biggest = i;
-    int left = 2*i + 1;
-    int right = 2*i + 2;
-
-    //logical statements
-    if (left < n && array[left] > array[biggest])
-    	biggest = left;
-    if (right < n && array[right] > array[biggest])
-	biggest = right;
-    
-    if (biggest != i)
-    {
-        swap(array[i], array[biggest]);
-	//recursion
-	heapify(array, n, biggest);
-    }
-}
-
-//main heapSort function
-void heapSort(int * array, int n)
-{
-    //build the heap
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(array, n, i);
-
-    for (int i = n-1; i >= 0; i--)
-    {
-        //move the current root to the end
-	swap(array[0], array[i]);
-	heapify(array, i, 0);
-    }
-}
-
-void countSort(int * array, int size)
-{
-    int maxValue = 0;
-    //find the max value first
-    for (int x = 1; x < size; x++)
-    {
-        if (array[x] > maxValue)
-            maxValue = array[x];
-    }
-
-    int sorted[size] = {0}; 
-    int count[maxValue] = {0};
-
-    for (int j = 0; j < size; j++)
-    {
-        count[array[j]]++;
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        count[i] += count[i-1];
-    }
-
-    for (int z = size; z >= 0; z--)
-    {
-        sorted[count[array[z]]] = array[z];
-	count[array[z]] = count[array[z]]-1;
-    }
-}
-
-
--- Sorting.h
+++ Sorting.h
@@ -6,6 +6,24 @@
 
 using namespace std;
 
+//algorithms (modify the definitions if needed)
+
+//heap sort functions
+void heapify(int * array, long int n, long int i);
+void heapSort(int * array, long int n);
+
+void countSort(int * array, long int n);
+
+void treeSort(int* array, long int n);
+
+void cubeSort(int* array);
+
+void quickSort(int* array);
+
+void quickSort2(int* array);
+
+void mergeSort(int* array);
+
 class Timer
 {
 public:
